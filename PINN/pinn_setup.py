6aae5a962dd144b3279795ccdea3b4262c98c943
Chiara0214 <chiara02.caselli@edu.unife.it>
2026-03-23 09:21:40 +0100

    changed helmholtz implementation to inhomogeneous

--- PINN/pinn_setup.py
+++ PINN/pinn_setup.py
@@ -1,7 +1,7 @@
 """ 
 Current implementation uses only source 1 and height 100, modeled as a 2D problem
 It takes x,y coordinates as input and outputs the predicted real and imaginary part of the FFT at the specified target frequency
-TODO: split data into test/train, add Robin boundary conditions, fix loss functions and weights, look into activation function and number of iterations
+TODO: split data into test/train, add Robin boundary conditions, look into activation function, number of iterations and loss weights, normalize by converting to dB
 """
 import os
 import torch
@@ -16,8 +16,10 @@ c = 343.0 #m/s
 omega = 2 * np.pi * target_freq
 k = omega / c
 
-def pde(x, y):
-    y0, y1 = y[:, 0:1], y[:, 1:2]
+#Helmholtz PDE implementation from https://deepxde.readthedocs.io/en/latest/demos/pinn_forward/helmholtz.2d.sound.hard.abc.html#helmholtz-sound-hard-scattering-problem-with-absorbing-boundary-conditions
+#inhomogeneous helmholtz equation because the source is inside the domain
+def pde(x, y): #here x is the input (x and y coordinates) of the model and y the output (pressure)
+    y0, y1 = y[:, 0:1], y[:, 1:2] #y0 is the real part of the pressure, y1 the imaginary part
 
     y0_xx = dde.grad.hessian(y, x, component=0, i=0, j=0)
     y0_yy = dde.grad.hessian(y, x, component=0, i=1, j=1)
@@ -25,8 +27,14 @@ def pde(x, y):
     y1_xx = dde.grad.hessian(y, x,component=1, i=0, j=0)
     y1_yy = dde.grad.hessian(y, x,component=1, i=1, j=1)
 
-    return [-y0_xx - y0_yy - k ** 2 * y0,
-            -y1_xx - y1_yy - k ** 2 * y1]
+    #f = delta(x-x0) models a point source at location x0, source: https://arxiv.org/pdf/1712.06091
+    x0, y0 = 0.17, 7.53
+    sigma = 0.1
+    dist = (x[:, 0:1] - x0)**2 + (x[:, 1:2] - y0)**2
+    f = (1 / (sigma * np.sqrt(2 * np.pi))) * torch.exp(-0.5 * dist / sigma**2) #dirac delta approximation with gaussian with mean 0 and small std sigma
+
+    return [-y0_xx - y0_yy - k ** 2 * y0 - f,
+            -y1_xx - y1_yy - k ** 2 * y1] #dirac delta has no imaginary part
 
 fs = 48000 #ISOBEL dataset sampling frequency
 directory = 'ISOBEL_SF_Dataset/Listening Room/ListeningRoom_SoundField_IRs/source_1/h_100/'
@@ -35,7 +43,7 @@ grid, approximated_freq = create_FFT_grid(directory, fs, target_freq)
 
 #The input array X_train needs to be an array of (x,y) real-life coordinates in meters
 #We need to convert grid indexes into meters using the room dims, and then reshape them into (x,y) coords
-l_x, l_y = 7.11, 5.43 #room dimensions in meters
+l_x, l_y = 4.14, 7.80 #room dimensions in meters
 n_x, n_y = grid.shape #32x32 grid
 
 x_vals = np.linspace(0, l_x, n_x)
@@ -44,11 +52,11 @@ y_vals = np.linspace(0, l_y, n_y)
 X, Y = np.meshgrid(x_vals, y_vals, indexing='ij')
 X_train = np.vstack((X.flatten(), Y.flatten())).T.astype(np.float32)
 
-#Reshapes grid values to get real and imaginary parts into two different arrays
+#Reshape grid values to get real and imaginary parts into two different arrays
 y_train_real = np.real(grid).flatten().reshape(-1, 1).astype(np.float32)
 y_train_imag = np.imag(grid).flatten().reshape(-1, 1).astype(np.float32)
 
-#Adds observed (Isobel) data as boundary conditions
+#Add observed (Isobel) data as boundary conditions
 #https://github.com/lululxvi/deepxde/issues/1952#issuecomment-2724018030
 bc_data_real = dde.icbc.PointSetBC(X_train, y_train_real, component=0)
 bc_data_imag = dde.icbc.PointSetBC(X_train, y_train_imag, component=1)
@@ -67,6 +75,5 @@ data = dde.data.PDE(
 net = dde.nn.FNN([2] + [50] * 3 + [2], "tanh", "Glorot uniform")
 model = dde.Model(data, net)
 
-#TODO: specify different loss functions for PDE and data, look into weights
-model.compile("adam", lr=1e-3, loss_weights=[1, 1, 100, 100]) #loss_weights = PDE real, PDE imaginary, data real, data imaginary
+model.compile("adam", lr=1e-3, loss="MSE", loss_weights=[1, 1, 100, 100]) #loss_weights = PDE real, PDE imaginary, data real, data imaginary
 losshistory, train_state = model.train(iterations=5000)
