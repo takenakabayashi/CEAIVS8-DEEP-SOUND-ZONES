6302bcb078eb97877d05b558d196a9907bd11041
Chiara0214 <chiara02.caselli@edu.unife.it>
2026-04-08 14:25:18 +0200

    added pinn implementation for multiple rooms

--- PINN/data_extraction.py
+++ PINN/data_extraction.py
@@ -92,6 +92,92 @@ def magnitude_phase_plots(grid, approximated_freq, heights, block=True):
 
     plt.show(block=block)
 
+#X should be an array of [x,y,z,lx,ly,lz,sx,sy,sz] coordinates normalized between 0 and 1 (point coordinates, room dimensions and source coordinates)
+def extract_data():
+    LR = {
+        "directory": "ISOBEL_SF_Dataset/Listening Room/ListeningRoom_SoundField_IRs/",
+        "sources_positions": [(0.17, 7.53, 1.0), (1.42, 2.08, 1.0)], #source_1 and source_2
+        "room_dimensions": (4.14, 7.80, 2.78),
+        "heights": [100, 130, 160, 190]
+        }
+
+    VR = {
+        "directory": "ISOBEL_SF_Dataset/VR Lab/VRLab_SoundField_IRs/",
+        "sources_positions": [(6.65, 7.93, 1.0), (5.23, 3.49, 1.0)], #source_1 and source_2
+        "room_dimensions": (6.98, 8.12, 3.03),
+        "heights": [100, 130, 160, 190]
+        }
+    
+    PR = {
+        "directory": "ISOBEL_SF_Dataset/Product Room/ProductRoom_SoundField_IRs/",
+        "sources_positions": [(0.32, 0.22, 1.0), (4.48, 4.81, 1.0)], #source_1 and source_2
+        "room_dimensions": (9.13, 12.03, 2.60),
+        "heights": [130, 160, 190]
+        }
+    
+    """ RB = {
+        "directory": "ISOBEL_SF_Dataset/Room B/RoomB_SoundField_IRs/",
+        "sources_positions": [(), ()], #source_1 and source_2
+        "room_dimensions": (4.16, 6.46, 2.30),
+        "heights": [100] 
+    } """
+
+    rooms = [LR, VR, PR] #check sources z coordinate
+
+    fs = 48000 #ISOBEL dataset sampling frequency
+    target_freq = 41 #Hz
+
+    X_tot = []
+    y_tot = []
+
+    for room in rooms:
+        dir = room["directory"]
+        heights = room["heights"]
+
+        if not os.path.isdir(dir):
+            raise ValueError(f"Directory {dir} does not exist")
+        
+        for source_idx, source_pos in enumerate(room["sources_positions"]):
+            source_path = os.path.join(dir, f"source_{source_idx+1}")
+            if not os.path.isdir(source_path):
+                raise ValueError(f"Directory {source_path} does not exist")
+        
+            l_x, l_y, l_z = room["room_dimensions"] #in meters
+
+            x_vals = np.linspace(0, 1, 32)
+            y_vals = np.linspace(0, 1, 32)
+            z_vals = np.array(heights) / (100.0 * l_z)
+
+            X, Y, Z = np.meshgrid(x_vals, y_vals, z_vals, indexing='ij')
+            X_flat = X.flatten()
+            Y_flat = Y.flatten()
+            Z_flat = Z.flatten()
+
+            n_points = len(X_flat)
+
+            x_dim = np.full(n_points, l_x)
+            y_dim = np.full(n_points, l_y)
+            z_dim = np.full(n_points, l_z)
+
+            sx = np.full(n_points, source_pos[0])
+            sy = np.full(n_points, source_pos[1])
+            sz = np.full(n_points, source_pos[2])
+
+            X_coords = np.vstack((X_flat, Y_flat, Z_flat, x_dim, y_dim, z_dim, sx, sy, sz)).T.astype(np.float32)
+            
+            grid, approximated_freq = create_FFT_grid(source_path, fs, target_freq, heights)
+
+            y_target = grid.flatten().reshape(-1, 1).astype(np.complex64)
+
+            X_tot.append(X_coords)
+            y_tot.append(y_target)
+        
+    X_tot = np.concatenate(X_tot, axis=0)
+    y_tot = np.concatenate(y_tot, axis=0)
+    
+    return X_tot, y_tot
+
+
 if __name__ == "__main__":
     
     fs = 48000 #Isobel sampling frequency
@@ -99,7 +185,9 @@ if __name__ == "__main__":
     #heights = [100]
     heights = [100, 130, 160, 190]
 
-    directory = 'ISOBEL_SF_Dataset/Listening Room/ListeningRoom_SoundField_IRs/source_1/'
+    X, y = extract_data()
+
+    """ directory = 'ISOBEL_SF_Dataset/Listening Room/ListeningRoom_SoundField_IRs/source_1/'
     
     grid, approximated_freq = create_FFT_grid(directory, fs, target_freq, heights)
-    magnitude_phase_plots(grid, approximated_freq, heights)
+    magnitude_phase_plots(grid, approximated_freq, heights) """
---
+++ PINN/pinn_multiple.py
@@ -0,0 +1,113 @@
+""" 
+Uses all sources and all heights, modeled as a 3D problem
+It takes normalized x,y,z coordinates and room dimensions as input, output is the predicted real and imaginary part of the FFT at the specified target frequency.
+TODO: add Robin boundary conditions, look into activation function, number of iterations and loss weights, normalize by converting to dB
+"""
+import os
+from sklearn.model_selection import train_test_split
+import torch
+os.environ["DDE_BACKEND"] = "pytorch"
+import deepxde as dde
+import numpy as np
+
+from data_extraction import extract_data
+
+target_freq = 40 #Hz
+c = 343.0 #m/s
+omega = 2 * np.pi * target_freq
+k = omega / c
+
+def nmse_db(y_true, y_pred):
+    num = np.sum(np.abs(y_true - y_pred)**2)
+    denom = np.sum(np.abs(y_true)**2)
+    
+    eps = 1e-12 #avoids division by 0
+    nmse = num / (denom + eps)
+    
+    nmse_db = 10 * np.log10(nmse + eps) #dB conversion
+    
+    return nmse_db
+
+#Helmholtz PDE implementation from https://deepxde.readthedocs.io/en/latest/demos/pinn_forward/helmholtz.2d.sound.hard.abc.html#helmholtz-sound-hard-scattering-problem-with-absorbing-boundary-conditions
+#inhomogeneous helmholtz equation because the source is inside the domain
+def pde(x, y):  #here x is the input (x and y coordinates) of the model and y the output (pressure)
+    Lx = x[:, 3:4]
+    Ly = x[:, 4:5]
+    Lz = x[:, 5:6]
+
+    y0, y1 = y[:, 0:1], y[:, 1:2] #y0 is the real part of the pressure, y1 the imaginary part
+
+    # divide by dimension^2 because of previous normalization (chain rule)
+    y0_xx = dde.grad.hessian(y, x, component=0, i=0, j=0) / (Lx ** 2)
+    y0_yy = dde.grad.hessian(y, x, component=0, i=1, j=1) / (Ly ** 2)
+    y0_zz = dde.grad.hessian(y, x, component=0, i=2, j=2) / (Lz ** 2)
+
+    y1_xx = dde.grad.hessian(y, x, component=1, i=0, j=0) / (Lx ** 2)
+    y1_yy = dde.grad.hessian(y, x, component=1, i=1, j=1) / (Ly ** 2)
+    y1_zz = dde.grad.hessian(y, x, component=1, i=2, j=2) / (Lz ** 2)
+
+    # physical source distance in meters
+    abs_x = x[:, 0:1] * Lx
+    abs_y = x[:, 1:2] * Ly
+    abs_z = x[:, 2:3] * Lz
+
+    #f = delta(x-xs) models a point source at location xs, source: https://arxiv.org/pdf/1712.06091
+    xs, ys, zs = x[:, 6:7], x[:, 7:8], x[:, 8:9] #source coordinates in meters
+    sigma = 0.1
+    dist = (abs_x - xs)**2 + (abs_y - ys)**2 + (abs_z - zs)**2
+    f = (1 / ((sigma * np.sqrt(2 * np.pi)) ** 3)) * torch.exp(-0.5 * dist / sigma**2) 
+
+    return [-y0_xx - y0_yy - y0_zz - k ** 2 * y0 - f,
+            -y1_xx - y1_yy - y1_zz - k ** 2 * y1]
+
+X, y = extract_data()
+
+# Split train/test
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+y_train_real = np.real(y_train).astype(np.float32)
+y_train_imag = np.imag(y_train).astype(np.float32)
+
+#Add observed (Isobel) data as boundary conditions
+#https://github.com/lululxvi/deepxde/issues/1952#issuecomment-2724018030
+bc_data_real = dde.icbc.PointSetBC(X_train, y_train_real, component=0)
+bc_data_imag = dde.icbc.PointSetBC(X_train, y_train_imag, component=1)
+
+#https://github.com/lululxvi/deepxde/issues/1762#issuecomment-2158327633
+geom = dde.geometry.geometry_nd.Hypercube(xmin=[0] * 9, xmax=[1] * 9)
+
+data = dde.data.PDE(
+    geom,
+    pde,
+    [bc_data_real, bc_data_imag],
+    num_domain=0, #evaluates physics strictly on the coordinates specified in anchors
+    num_boundary=0, 
+    anchors=X
+)
+
+net = dde.nn.FNN([9] + [50] * 3 + [2], "tanh", "Glorot uniform")
+model = dde.Model(data, net)
+
+model.compile("adam", lr=1e-3, loss="MSE", loss_weights=[1, 1, 60, 60]) 
+losshistory, train_state = model.train(iterations=5000)
+
+#Model evaluation
+y_pred = model.predict(X_test)
+y_pred_real, y_pred_imag = y_pred[:, 0], y_pred[:, 1]
+
+y_test_real = np.real(y_test).flatten()
+y_test_imag = np.imag(y_test).flatten()
+
+test_mse_real = np.mean((y_test_real - y_pred_real)**2)
+test_mse_imag = np.mean((y_test_imag - y_pred_imag)**2)
+print(f"Test MSE real: {test_mse_real:.6f}")
+print(f"Test MSE imaginary: {test_mse_imag:.6f}")
+
+#NMSE
+y_pred = y_pred_real + 1j * y_pred_imag
+y_test = y_test_real + 1j * y_test_imag
+
+nmse = nmse_db(y_test, y_pred)
+print(f"Test NMSE: {nmse:.2f} dB")
